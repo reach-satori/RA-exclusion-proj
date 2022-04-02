7d8f594ff28135fb338843f7cbe187605a3a8a59
Mateus Reis <mateus.sureis@gmail.com>
2022-04-02 14:00:09 +0300

    state change to its own function, input

--- proj.py
+++ proj.py
@@ -4,8 +4,25 @@ from random import randint
 from rpyc.utils.server import ThreadedServer
 from rpyc import Service, connect
 from time import perf_counter
+from enum import Enum
+
+class KeyboardThread(Thread):  # threaded input lifted from a stackexchange qst.
+
+    def __init__(self, input_cbk = None, name='keyboard-input-thread'):
+        self.input_cbk = input_cbk
+        super(KeyboardThread, self).__init__(name=name)
+        self.start()
+
+    def run(self):
+        while True:
+            self.input_cbk(input())  # waits to get input + Return
+
+def command_callback(command):
+    print(command)
+
 
 class ProcessService(Service):
+
     def __init__(self, procid):
         self.procs = {}
         self.state = "DO-NOT-WANT"
@@ -14,36 +31,55 @@ class ProcessService(Service):
         self.clock = perf_counter()  # this one is only for rough times, for timeouts
         self.deferred_actions = []
 
-        self.wanted_timeout = (5, 5)  # default values
+        self.do_not_want_timeout = (5, 5)  # default values
         self.held_timeout = (10, 10)
-        self.current_timeout = randint(*self.wanted_timeout)
+        self.current_timeout = randint(*self.do_not_want_timeout)
         self.acks = []
 
         print(f"instantiating {self.procid}")
 
     def request_cs(self):
-        for i, procid in enumerate(self.procs.keys()):
+        for procid in self.procs:
             if self.send_request(procid):
-                self.acks[i] = True
+                self.acks[procid] = True
 
     def release_cs(self):
         # run deferred responses
         pass
 
-    def tick(self):
-        t = perf_counter() - self.clock
-        if t > self.current_timeout:
-            if self.state == "DO_NOT_WANT":
-                self.state = "WANTED"
-                self.request_cs()
-            elif self.state == "HELD":
-                self.release_cs()
-                self.state = "DO_NOT_WANT"
+    def statechange(self, newstate):
+        self.clock = perf_counter()
+        self.lamp_clock += 1
+        if newstate == "WANTED":
+            self.state = "WANTED"
+            self.request_cs()
+            self.current_timeout = float("inf")
 
-        if self.state == "WANTED" and all(self.acks):
+        elif newstate == "DO-NOT-WANT":
+            self.release_cs()
+            self.state = "DO-NOT-WANT"
+            self.current_timeout = randint(*self.do_not_want_timeout)
+
+        elif newstate == "HELD":
             self.state = "HELD"
             self.current_timeout = randint(*self.held_timeout)
-            self.acks = [False] * len(self.procs)
+            self.acks = {procid: False for procid in self.procs}
+
+        else:
+            raise ValueError("Malformed state")
+
+    def tick(self):
+        t = perf_counter() - self.clock
+        print(self.procid, self.state)
+        if t > self.current_timeout:
+            print(t, self.procid, self.state, id(self))
+            if self.state == "DO-NOT-WANT":
+                self.statechange("WANTED")
+            elif self.state == "HELD":
+                self.statechange("DO-NOT-WANT")
+
+        if self.state == "WANTED" and all(self.acks.values()):
+            self.statechange("HELD")
 
     def send_request(self, procid):
         self.lamp_clock += 1
@@ -63,12 +99,7 @@ class ProcessService(Service):
     def exposed_init_fellow_procs(self, procs):
         self.procs = dict(procs)
         self.procs.pop(self.procid)  # only other processes
-        self.acks = [False] * len(self.procs)
-
-    def exposed_connect_to(self, id, port):
-        c = connect("localhost", port)
-        print(f"connected to port {port} from proc {self.procid} to {id}")
-        c.close()
+        self.acks = {procid: False for procid in self.procs}
 
     def __del__(self):
         print(f"destroying {self.procid}")
@@ -78,8 +109,7 @@ if __name__ == "__main__":
     num_procs = int(sys.argv[1])
     procs = dict()  # dict of id: server object
     for i in range(num_procs):  # create the nodes
-        service = ProcessService(i)
-        proc = ThreadedServer(service, port=0)
+        proc = ThreadedServer(ProcessService(i), port=0)
         procs[i] = proc  # we assume localhost for everything
 
     proc_port_dict = {key: proc.port for key, proc in procs.items()}
@@ -90,7 +120,7 @@ if __name__ == "__main__":
         threads.append(t)
         t.start()
 
-    for id, port in proc_port_dict.items():
+    for pid, port in proc_port_dict.items():
         c = connect("localhost", port)
         # since we're using port 0 rather than specifying, we need to tell the processes
         # the ports for every other process
@@ -98,12 +128,11 @@ if __name__ == "__main__":
         c.root.init_fellow_procs(tuple(proc_port_dict.items()))
         c.close()
 
-    c = connect("localhost", proc_port_dict[2])
-    c.root.connect_to(id, proc_port_dict[1])
+    kthread = KeyboardThread(command_callback)
 
     while procs:
         for server in procs.values():
-            proc.service.tick()
+            server.service.tick()
 
         for thread in threads:
-            thread.join(timeout=1)
+            thread.join(timeout=(1/len(procs)))
