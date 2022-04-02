cdcad3c3c8f25c6a0850967b951e2107de9557b7
Mateus Reis <mateus.sureis@gmail.com>
2022-04-02 20:47:25 +0300

    just cleaning up

--- proj.py
+++ proj.py
@@ -1,9 +1,11 @@
 import sys
 from threading import Thread
 from random import randint
+from time import perf_counter
+from datetime import datetime
+
 from rpyc.utils.server import ThreadedServer
 from rpyc import Service, connect
-from time import perf_counter
 
 class KeyboardThread(Thread):  # threaded input lifted from a stackexchange qst.
 
@@ -17,19 +19,33 @@ class KeyboardThread(Thread):  # threaded input lifted from a stackexchange qst.
             self.input_cbk(input())  # waits to get input + Return
 
 def command_callback(command):
-    print(command)
+    global procs
+    command = command.lower().strip().split()
+    print(f"command '{command[0]}' called")
+    if command[0] == "list":
+        for proc in procs.values():
+            print(f"P{proc.service.procid}: {proc.service.state}")
+    elif command[0] == "time-cs":
+        assert int(command[1]) >= 10
+        for proc in procs.values():
+            proc.service.do_not_want_timeout[1] = int(command[1])
+    elif command[0] == "time-p":
+        assert int(command[1]) >= 5
+        for proc in procs.values():
+            proc.service.held_timeout[1] = int(command[1])
+
+    else:
+        print(f"command {command[0]} invalid")
+
 
 class ProcessService(Service):
 
-    # this isn't actually used anywhere other than testing, for print string formatting
-    statedict = {"DO-NOT-WANT": "WANTED", "WANTED": "HELD", "HELD": "DO-NOT-WANT"}
-
-    def __init__(self, procid, verbose=False):
+    def __init__(self, procid, verbose = False):
         self.verbose = verbose
         self.procs = {}  # dict of proc_ids : ports (doesn't include self)
         self.state = "DO-NOT-WANT"
         self.procid = procid
-        self.lamp_clock = 0 # lamport
+        self.lamp_clock = 0  # lamport
         self.clock = perf_counter()  # only for counting its own timeouts
         # list of procids: release_cs empties this list and sends deferred acks
         self.deferred_actions = []
@@ -42,10 +58,11 @@ class ProcessService(Service):
         # self.held_timeout = (randint(5,15), randint(15,35))
 
         self.current_timeout = randint(*self.do_not_want_timeout)
-        self.acks = []
-
-        if self.verbose:
-            print(f"instantiating process {self.procid}")
+        self.acks = {}  # acks is a dict of pid: bool size (nprocs - 1) that, when the state is
+        # WANTED, displays which processes have responded positively to the request already.
+        # when all values in acks are True, the process grabs the critical section and changes
+        # state to HELD
+        self.log(f"instantiating process {self.procid}")
 
     def request_cs(self):
         # run when a process decides it wants the CS. immediate acks are recorded
@@ -62,15 +79,19 @@ class ProcessService(Service):
     def send_ack(self, procid):
         connect("localhost", self.procs[procid]).root.receive_ack(self.procid)
 
-    def statechange(self, newstate):
+    def log(self, text):
         if self.verbose:
-            print(f"proc {self.procid} changing state to {newstate}")
+            timestamp = datetime.now().strftime("%H:%M:%S.%f")
+            print(timestamp,"|", text)
+
+    def statechange(self, newstate):
+        self.log(f"P{self.procid} changing state to {newstate}")
         self.clock = perf_counter()
         self.lamp_clock += 1
         if newstate == "WANTED":
             self.state = "WANTED"
             self.request_cs()
-            self.current_timeout = float("inf")
+            self.current_timeout = float("inf") # while WANTED, timeouts are suspended
 
         elif newstate == "DO-NOT-WANT":
             self.release_cs()
@@ -88,8 +109,12 @@ class ProcessService(Service):
     def tick(self):
         t = perf_counter() - self.clock
         if t > self.current_timeout:
-            if self.verbose:
-                print(f"timeout reached in proc {self.procid}")
+
+            msg = f"timeout reached in P{self.procid} with state {self.state}"
+            if self.state != "WANTED":
+                msg += f" after {self.current_timeout} seconds"
+            self.log(msg)
+
             if self.state == "DO-NOT-WANT":
                 self.statechange("WANTED")
             elif self.state == "HELD":
@@ -110,10 +135,13 @@ class ProcessService(Service):
         if self.state == "DO-NOT-WANT" or (self.state == "WANTED" and timestamp < self.lamp_clock):
             out = True
         else:
+            # if out is false, the receiving process defers the response
             out = False
             self.deferred_actions.append(sending_procid)
 
         self.lamp_clock = max(self.lamp_clock+1, timestamp)
+        # if out is true, this models an immediate response. Although there is no additional
+        # connect() call back
         return out
 
     def exposed_init_fellow_procs(self, procs):
@@ -121,15 +149,12 @@ class ProcessService(Service):
         self.procs.pop(self.procid)  # only other processes
         self.acks = {procid: False for procid in self.procs}
 
-    def __del__(self):
-        print(f"destroying {self.procid}")
-
 
 if __name__ == "__main__":
     num_procs = int(sys.argv[1])
     procs = dict()  # dict of id: server object
     for i in range(num_procs):  # create the nodes
-        proc = ThreadedServer(ProcessService(i, verbose=False), port=0)
+        proc = ThreadedServer(ProcessService(i, verbose=True), port=0)
         procs[i] = proc  # we assume localhost for everything
 
     proc_port_dict = {key: proc.port for key, proc in procs.items()}
